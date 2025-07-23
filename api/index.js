b61c1d0bfa4f66c5fe70b656c47555c83e566309
Prashant-7718 <prashant@mermaidchart.com>
2025-07-23 20:17:15 +0530

    removed unwanted changes

--- api/index.js
+++ api/index.js
@@ -134,7 +134,6 @@ if (devEnv) app.use(errorHandler());
 // Wire up routes
 routes(app, addon);
 
-// For Vercel serverless deployment, export as default
 export default app;
 
 function redactJwtTokens(req) {
